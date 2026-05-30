ce56574a4900f14eeea866a7fa8d8f21d291fb1c
Adarsh-G-Neelgund <151861153+Adarsh-G-Neelgund@users.noreply.github.com>
2026-05-30 19:44:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(page_title="Flood Predictor", layout="centered")
-st.title(" 🌊AI-Based Flood Prediction System using Environmental Parameters")
+st.title(" 🌊AI-Based Flood Prediction System ")
 
 
 @st.cache_resource
